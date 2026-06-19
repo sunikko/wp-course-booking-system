e841dc13f340defc0f0cd052924ca3f1c1f3bc7a
Soy <sunikko77@gmail.com>
2026-06-19 01:15:26 +0100

    add basic theme header/footer structure

---
+++ wp/wp-content/themes/my-course-theme/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p>Course Booking System-F</p>
+</footer>
+
+</body>
+</html>
---
+++ wp/wp-content/themes/my-course-theme/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php bloginfo('name'); ?></title>
+</head>
+<body>
+
+<header>
+    <h1><?php bloginfo('name'); ?></h1>
+</header>
