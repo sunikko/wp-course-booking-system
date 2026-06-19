4cfcdc988b9b42858ba891d7ac41fa03cbbb311b
Soy <sunikko77@gmail.com>
2026-06-19 01:54:03 +0100

    add course custom post type

--- wp/wp-content/themes/my-course-theme/functions.php
+++ wp/wp-content/themes/my-course-theme/functions.php
@@ -6,3 +6,20 @@ function my_theme_setup()
 }
 
 add_action('after_setup_theme', 'my_theme_setup');
+
+
+function register_courses_post_type()
+{
+    register_post_type('course', [
+        'labels' => [
+            'name' => 'Courses',
+            'singular_name' => 'Course'
+        ],
+        'public' => true,
+        'has_archive' => true,
+        'supports' => ['title', 'editor'],
+        'menu_icon' => 'dashicons-welcome-learn-more'
+    ]);
+}
+
+add_action('init', 'register_courses_post_type');
