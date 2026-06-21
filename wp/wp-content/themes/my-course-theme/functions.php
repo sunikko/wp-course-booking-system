5d6fb2bdaf5df11c25ba418eb7a7f5974f11d3e1
Soy <sunikko77@gmail.com>
2026-06-21 15:16:22 +0100

    refactor: Organise booking page assets js css

--- wp/wp-content/themes/my-course-theme/functions.php
+++ wp/wp-content/themes/my-course-theme/functions.php
@@ -48,10 +48,23 @@ add_action('init', function () {
 function my_theme_assets() {
     wp_enqueue_style(
         'booking-style',
-        get_template_directory_uri() . '/assets/booking.css',
+        get_template_directory_uri() . '/assets/css/booking.css',
         array(),
         '1.0'
     );
 }
 
 add_action('wp_enqueue_scripts', 'my_theme_assets');
+
+
+add_action('wp_enqueue_scripts', function () {
+
+    wp_enqueue_script(
+        'booking-js',
+        get_template_directory_uri() . '/assets/js/booking.js',
+        array(),
+        '1.0',
+        true // footer load (IMPORTANT)
+    );
+
+});
