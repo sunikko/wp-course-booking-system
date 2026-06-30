18a507995fa89a956895a039c10f98688c0bf13d
Soy <sunikko77@gmail.com>
2026-06-30 22:54:02 +0100

    Refactor: clean up - Remove unused single-course.php and old booking code

--- wp/wp-content/themes/my-course-theme/functions.php
+++ wp/wp-content/themes/my-course-theme/functions.php
@@ -1,5 +1,4 @@
 <?php
-ob_start();
 
 function my_theme_setup()
 {
@@ -28,22 +27,7 @@ function register_courses_post_type()
 
 add_action('init', 'register_courses_post_type');
 
-add_action('init', function () {
 
-    if (isset($_POST['book_course'])) {
-
-        $course_id = $_POST['course_id'];
-
-        $capacity = get_field('capacity', $course_id);
-
-        if ($capacity > 0) {
-            update_field('capacity', $capacity - 1, $course_id);
-        }
-
-        wp_redirect(get_permalink($course_id));
-        exit;
-    }
-});
 
 function my_theme_assets()
 {
